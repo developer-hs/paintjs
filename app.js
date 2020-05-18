892d6f9049ac389887ee6e228773b87c15ebcb6f
gygy2006 <gygy2006@naver.com>
2020-05-19 00:13:11 +0900

    canvas 최종

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+let rangeWrite = document.getElementById("jsRangeWrite");
+const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
-ctx.strokeStyle = "#2c2c2c";
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.height, canvas.width); // canvas 의 첫 background 색상을 white 로 지정
+ctx.strokeStyle = "#INITIAL_COLOR";
+ctx.fillStyle = "INITIAL_COLOR";
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
   painting = false;
@@ -17,21 +31,76 @@ function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
   if (!painting) {
-    ctx.beginPath();
-    ctx.moveTo(x, y);
+    ctx.beginPath(); // 이전에 그린 경로를 초기화하며 경로를 시작
+    ctx.moveTo(x, y); // 시작점
   } else {
     ctx.lineTo(x, y);
     ctx.stroke();
   }
 }
 
-function onMouseDown(event) {
-  painting = true;
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+  ctx.fillStyle = color;
 }
 
+function handleRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+  rangeWrite.innerHTML = event.target.value;
+}
+
+function handleModeClick(event) {
+  if (filling == true) {
+    filling = false;
+    mode.innerText = "Fill";
+  } else {
+    filling = true;
+    mode.innerText = "PAINT";
+  }
+}
+
+function handleCanvasClick(event) {
+  if (filling) {
+    ctx.fillRect(0, 0, canvas.height, canvas.width);
+  }
+}
+
+function handleCM(event) {
+  event.preventDefault();
+}
+
+function handleSaveClick(event) {
+  const image = canvas.toDataURL(""); // image 를 저장한 URL 을 반환
+  // Default = PNG , JPG = image/jpeh
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = "PaintJs[EXPORT✍✍]"; // download 는 a 태그의 atrribute
+  // <a download="파일명"/> : browser 에게 링크로 가지말고 다운로드 하라고 지시
+  link.click();
+}
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", startPainting); // 마우스 클릭시
+  canvas.addEventListener("mousedown", startPainting); // 마우스 클릭을 누르고 마우스에서 손가락을 떼지 않은 상태
   canvas.addEventListener("mouseup", stopPainting); // 마우스 에서 손 땠을때
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("mousedown", handleCanvasClick);
+  canvas.addEventListener("contextmenu", handleCM);
+}
+
+Array.from(colors).forEach((color) =>
+  color.addEventListener("click", handleColorClick)
+);
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
+
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
+}
+
+if (saveBtn) {
+  saveBtn.addEventListener("click", handleSaveClick);
 }
